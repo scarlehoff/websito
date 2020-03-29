7e231d99dc3d606cc3ae36e1b2734ab8c8196c7f
juacrumar <juacrumar@gmail.com>
2020-03-29 20:29:31 +0200

    add projects

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,11 @@ let router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
+
 router.get('/teaching', function(req, res, next) {
   res.render('teaching', { title: 'Teaching' });
 });
+
 router.get('/research', function(req, res, next) {
   const pubInfo = require('../data/publications.json');
   const articles = pubInfo["article"];
@@ -16,8 +18,10 @@ router.get('/research', function(req, res, next) {
   const talks = require('../data/talks.json');
   res.render('research', { title: 'Research', articles, software, talks });
 });
+
 router.get('/software', function(req, res, next) {
-  res.render('software', { title: 'Software' });
+  const projects = require('../data/projects.json');
+  res.render('software', { title: 'Software', projects});
 });
 
 
