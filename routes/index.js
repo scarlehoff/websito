552e6304a77df1ff4b161062620137fdbb8c32f5
juacrumar <juacrumar@gmail.com>
2020-04-10 17:18:41 +0200

    many changes to resume, many new icons

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,20 @@
 let express = require('express');
 let router = express.Router();
 
+// Parse icons
+const iconmap = {
+  'C++' : 'devicon-cplusplus-plain',
+  'python' : 'devicon-python-plain',
+  'javascript' : 'devicon-javascript-plain',
+  'Linux' : 'devicon-linux-plain',
+  'Vim' : 'devicon-vim-plain',
+  'Version control' : 'devicon-git-plain',
+  'GPU computing' : 'mdi mdi-expansion-card',
+  'Mathematica, Maple' : 'mdi mdi-math-integral-box',
+  'Latex & markdown' : 'mdi mdi-language-markdown',
+  'fortran' : 'mdi mdi-language-fortran',
+  'keras & tensorflow' : 'tf-plain',
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -29,7 +43,7 @@ router.get('/software', function(req, res, next) {
 
 router.get('/resume', function(req, res, next) {
   const resume = require('../data/resume.json');
-  res.render('resume', { title: 'Resume', resume });
+  res.render('resume', { title: 'Resume', resume, iconmap });
 });
 
 
