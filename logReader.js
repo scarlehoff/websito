fe3583178bc2ad258a6879c56df570eb9ea448c2
juacrumar <juacrumar@gmail.com>
2020-04-12 20:16:08 +0200

    add TODOs

--- logReader.js
+++ logReader.js
@@ -24,6 +24,8 @@ function checkCreate(folname) {
 checkCreate(logFolder);
 checkCreate(backupFolder);
 
+// TODO check the database exists, create if it doesn't
+
 /*
  * Look for new file,
  * read them up (later on, write up the data to a db)
@@ -41,7 +43,9 @@ watcher.on('add', (filename) => {
     fs.readFile(filename, (err, textContent) => {
       if (err) return console.log(err);
 
-      // Save to database
+      // TODO use the IP to get location information
+
+      // TODO save data to database
 
     });
     // Move the file out of the way
