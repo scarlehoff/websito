21b6db9fc385a666f2d4c2b3e8b0a0966869b0cc
juacrumar <juacrumar@gmail.com>
2020-04-16 21:43:58 +0200

    add helmet for protection

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var rfs = require('rotating-file-stream');
+// Use helmet to protect the http headers
+// as per https://expressjs.com/en/advanced/best-practice-security.html#use-helmet
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
