eca0a28ceceae1ba0f63a82085ccc3200d005ab4
juacrumar <juacrumar@gmail.com>
2020-09-20 16:55:31 +0200

    cleaned u[

--- views/todoapp.js
+++ views/todoapp.js
@@ -74,8 +74,46 @@ const authProvider = {
 const graphClient = MicrosoftGraph.Client.initWithMiddleware({authProvider});
 //-------------------- Graph client initialized
 
-//------------ User-space functions
+// --- Sign In API
+// Functions used for sign-in/out
+async function signIn() {
+  // Login
+  try {
+    // Use MSAL to login
+    const authResult = await msalClient.loginPopup(msalRequest);
+    console.log('id_token acquired at: ' + new Date().toString());
+    // Save the account username, needed for token acquisition
+    sessionStorage.setItem('msalAccount', authResult.account.username);
 
+    // Get the user's profile from Graph
+    user = await getUser();
+    // Save the profile in session
+    sessionStorage.setItem('graphUser', JSON.stringify(user));
+    updatePage(Views.home);
+  } catch (error) {
+    console.log(error);
+    updatePage(Views.error, {
+      message: 'Error logging in',
+      debug: error
+    });
+  }
+}
+function signOut() {
+  account = null;
+  sessionStorage.removeItem('graphUser');
+  msalClient.logout();
+}
+async function getUser() {
+  return await graphClient
+    .api('/me')
+    // Only get the fields used by the app
+    .select('id,displayName,mail,userPrincipalName,mailboxSettings')
+    .get();
+}
+// --------------------------------------------------------------------
+
+
+//------------ User-space functions
 //--- Getting the list of lists
 /* API call to Microsoft Graph to receive a list of all lists
  * in my MS To do account */
@@ -175,7 +213,6 @@ async function showTasks() {
   let listOfTasks = "";
   for (task of tasks) {
     let title = task.title;
-    let completionRaw = moment(task.completedDateTime.dateTime);
     listOfTasks += `<tr><th>${title}</th></tr>`;
   }
 
@@ -194,69 +231,12 @@ async function showTasks() {
   mainContainer.innerHTML = tableContent;
 }
 
-
-
-
-
-
-
-
-
-function createElement(type, className, text) {
-  var element = document.createElement(type);
-  element.className = className;
-
-  if (text) {
-    var textNode = document.createTextNode(text);
-    element.appendChild(textNode);
-  }
-
-  return element;
-}
-
-
-
-function showError(error) {
-  var alert = createElement('div', 'alert alert-danger');
-
-  var message = createElement('p', 'mb-3', error.message);
-  alert.appendChild(message);
-
-  if (error.debug)
-  {
-    var pre = createElement('pre', 'alert-pre border bg-light p-2');
-    alert.appendChild(pre);
-
-    var code = createElement('code', 'text-break text-wrap',
-      JSON.stringify(error.debug, null, 2));
-    pre.appendChild(code);
-  }
-
-  mainContainer.innerHTML = '';
-  mainContainer.appendChild(alert);
-}
-
-function showCalendar(events) {
-  // TEMPORARY
-  // Render the results as JSON
-  var alert = createElement('div', 'alert alert-success');
-
-  var pre = createElement('pre', 'alert-pre border bg-light p-2');
-  alert.appendChild(pre);
-
-  var code = createElement('code', 'text-break',
-    JSON.stringify(events, null, 2));
-  pre.appendChild(code);
-
-  mainContainer.innerHTML = '';
-  mainContainer.appendChild(alert);
-}
-
-function updatePage(view, data) {
-  if (!view) {
-    view = Views.home;
-  }
-
+/* Update page function
+ * This function is called every time some important event happens
+ * such as refresh or login
+ * and hides/shows the right buttons/elements
+ */
+function updatePage(view) {
   const user = JSON.parse(sessionStorage.getItem('graphUser'));
 
   if (user) {
@@ -272,108 +252,6 @@ function updatePage(view, data) {
     calendarElm.style.display = "none";
     showTasksButtonElm.style.display = "none";
   }
-
-  switch (view) {
-    case Views.error:
-      showError(data);
-      break;
-    case Views.home:
-      break;
-    case Views.calendar:
-      showCalendar(data);
-      break;
-  }
 }
-
-
 
 updatePage(Views.home);
-
-// ---  auth.js
-
-async function signIn() {
-  // Login
-  try {
-    // Use MSAL to login
-    const authResult = await msalClient.loginPopup(msalRequest);
-    console.log('id_token acquired at: ' + new Date().toString());
-    // Save the account username, needed for token acquisition
-    sessionStorage.setItem('msalAccount', authResult.account.username);
-
-    // Get the user's profile from Graph
-    user = await getUser();
-    // Save the profile in session
-    sessionStorage.setItem('graphUser', JSON.stringify(user));
-    updatePage(Views.home);
-  } catch (error) {
-    console.log(error);
-    updatePage(Views.error, {
-      message: 'Error logging in',
-      debug: error
-    });
-  }
-}
-function signOut() {
-  account = null;
-  sessionStorage.removeItem('graphUser');
-  msalClient.logout();
-}
-
-// --- graph.js
-// Create an authentication provider
-
-async function getUser() {
-  return await graphClient
-    .api('/me')
-    // Only get the fields used by the app
-    .select('id,displayName,mail,userPrincipalName,mailboxSettings')
-    .get();
-}
-
-async function getEvents() {
-  const user = JSON.parse(sessionStorage.getItem('graphUser'));
-
-  // Convert user's Windows time zone ("Pacific Standard Time")
-  // to IANA format ("America/Los_Angeles")
-  // Moment needs IANA format
-  let ianaTimeZone = "Etc/GMT";
-  console.log(`Converted: ${ianaTimeZone}`);
-
-  // Configure a calendar view for the current week
-  // Get midnight on the start of the current week in the user's timezone,
-  // but in UTC. For example, for Pacific Standard Time, the time value would be
-  // 07:00:00Z
-  let startOfWeek = moment.tz('America/Los_Angeles').startOf('week').utc();
-  // Set end of the view to 7 days after start of week
-  let endOfWeek = moment(startOfWeek).add(7, 'day');
-  console.log("This point");
-
-  try {
-    // GET /me/calendarview?startDateTime=''&endDateTime=''
-    // &$select=subject,organizer,start,end
-    // &$orderby=start/dateTime
-    // &$top=50
-    console.log("Started response");
-    let response = await graphClient
-      .api('/me/todo/lists').version('beta')
-//      .api('/me/calendarview') // This needs to change to /me/todo/lists to get the lists or /me/todo/lists/{listname}/tasks to get the tasks in a list
-      // Set the Prefer=outlook.timezone header so date/times are in
-      // user's preferred time zone
-//      .header("Prefer", `outlook.timezone="${user.mailboxSettings.timeZone}"`)
-//      // Add the startDateTime and endDateTime query parameters
-//      .query({ startDateTime: startOfWeek.format(), endDateTime: endOfWeek.format() })
-//      // Select just the fields we are interested in
-//      .select('subject,organizer,start,end')
-//      // Sort the results by start, earliest first
-//      .orderby('start/dateTime')
-//      // Maximum 50 events in response
-//      .top(50)
-      .get();
-    updatePage(Views.calendar, response.value);
-  } catch (error) {
-    updatePage(Views.error, {
-      message: 'Error getting events',
-      debug: error
-    });
-  }
-}
